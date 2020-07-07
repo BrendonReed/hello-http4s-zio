d493a363eab9d615c8709b4789e44502181942ba
Brendon Reed <brendon.reed@slalom.com>
2020-07-07 08:47:32 -0600

    init

---
+++ build.sbt
@@ -0,0 +1,24 @@
+scalaVersion := "2.13.2" // Also supports 2.12.x
+
+val http4sVersion = "0.21.5"
+lazy val doobieVersion = "0.8.8"
+val CirceVersion = "0.13.0"
+
+// Only necessary for SNAPSHOT releases
+resolvers += Resolver.sonatypeRepo("snapshots")
+
+libraryDependencies ++= Seq(
+    "org.http4s" %% "http4s-dsl" % http4sVersion,
+    "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+    "org.http4s" %% "http4s-blaze-client" % http4sVersion,
+    "org.http4s"      %% "http4s-circe"        % http4sVersion,
+    "io.circe"        %% "circe-generic"       % CirceVersion,
+    "dev.zio" %% "zio" % "1.0.0-RC12-1",
+    "dev.zio" %% "zio-interop-cats" % "2.0.0.0-RC2",
+    "org.tpolecat" %% "doobie-core" % doobieVersion,
+    "org.tpolecat" %% "doobie-postgres" % doobieVersion,
+    "org.tpolecat" %% "doobie-specs2" % doobieVersion
+)
+
+// Uncomment if you're using Scala 2.12.x
+// scalacOptions ++= Seq("-Ypartial-unification")
---
+++ src/Main/scala/Main.scala
@@ -0,0 +1,84 @@
+import org.http4s._
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.dsl.Http4sDsl
+import org.http4s.server.Router
+import zio.{Task, ZIO}
+import zio.interop.catz._
+import zio.interop.catz.implicits._
+import org.http4s.implicits._
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.middleware.Logger
+
+import doobie._
+import doobie.implicits._
+import io.circe.generic.auto._
+import org.http4s.circe.CirceEntityCodec._
+
+import scala.concurrent.ExecutionContext.global
+//implicit def countriesEncoder: EntityEncoder[Task, List[Country]] = ???
+object Main extends CatsApp {
+
+  //private val bookRepo: BookRepo = new BookRepo.DummyImpl
+
+  def bookRoutes(repo: CountriesPersistenceService): HttpRoutes[Task] = {
+    val dsl = new Http4sDsl[Task]{}
+    val allCountries: Task[List[Country]] = repo.getAll()
+    import dsl._
+    HttpRoutes.of[Task] {
+      case _@GET -> Root / "books" =>
+        Ok(allCountries)
+    }
+  }
+
+  //maps from HttpRoutes[F] -> HttpApp: Kleisli[Task, Request[Task], Response[Task]]
+  //to compose an entire HttpApp from a bunch of routes
+
+  def run(args: List[String]) =
+    streamz.compile.drain.fold(_ => 1, _ => 0)
+
+  def streamz() = {
+    val xa = Transactor.fromDriverManager[Task](
+      "org.postgresql.Driver",     // driver classname
+      "jdbc:postgresql:world",     // connect URL (driver-specific)
+      "postgres",                  // user
+      "",                          // password
+    )
+    for {
+      client <- BlazeClientBuilder[Task](global).stream
+      bookRepo = new CountriesPersistenceService(xa)
+
+      httpRoutes = Router[Task](
+        "/" -> bookRoutes(bookRepo)
+      ).orNotFound
+
+      httpApp = httpRoutes
+      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      exitCode <- BlazeServerBuilder[Task](global)
+          .bindHttp(9000, "0.0.0.0")
+          .withHttpApp(finalHttpApp)
+          .serve
+    } yield exitCode
+  }.drain
+}
+
+case class Country(code: String, name: String, pop: Int, gnp: Option[Double])
+final class CountriesPersistenceService(tnx: Transactor[Task]) {
+
+  import DAO._
+  def getAll(): Task[List[Country]] =
+    SQL
+      .getAll
+      .stream
+      .take(15)
+      .compile.toList
+      .transact(tnx)
+}
+//queries separated so they can be typechecked with doobie-specs2 or doobie-scalatest
+object DAO {
+  object SQL {
+    def getAll = {
+      sql"select code, name, population, gnp from country"
+        .query[Country]
+    }
+  }
+}
